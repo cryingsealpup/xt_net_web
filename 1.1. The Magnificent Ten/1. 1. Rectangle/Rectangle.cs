8238a96196e2a99326e686a6f2f5d3908c7339d0
EkaterinaRadchenko <ekatherinered@gmail.com>
2020-05-24 14:17:07 +0400

    Task 1.1: area of rectangle with a b sides

---
+++ 1.1. The Magnificent Ten/1. 1. Rectangle/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            int b = int.Parse(Console.ReadLine());
+
+            if (a <= 0 || b <= 0) Console.WriteLine("Incorrect values");
+
+            else Console.WriteLine("Area = " + a * b);
+
+            Console.ReadKey();
+        }
+    }
+}
