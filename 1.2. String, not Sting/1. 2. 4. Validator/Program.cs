4c83e02baebb8f5252eedf602d1428e4575f09cb
EkaterinaRadchenko <ekatherinered@gmail.com>
2020-05-26 22:20:56 +0400

    added 1.2.4

---
+++ 1.2. String, not Sting/1. 2. 4. Validator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Input the string:");
+            string str = Console.ReadLine();*/
+            // or test example
+            string str = "я плохо учил русский язык. забываю " +
+                "начинать предложения с заглавной. хорошо, что " +
+                "можно написать программу!"; 
+            string result = "";
+            string Separators = ".!?";
+            bool IsSentence = true;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (IsSentence && char.IsLetter(str[i]))
+                {
+                    result += char.ToUpper(str[i]);
+                    IsSentence = false;
+                }
+                else result += str[i];
+                if (Separators.Contains(str[i])) IsSentence = true;
+            }
+
+            Console.WriteLine("String with capitalized letters: \n" + result);
+
+            Console.ReadKey();
+        }
+    }
+}
