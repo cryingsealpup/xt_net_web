13e075a35b4db824a7dcc9cc9545e740c7764799
EkaterinaRadchenko <ekatherinered@gmail.com>
2020-05-26 21:54:47 +0400

    Added 1.2.3

---
+++ 1.2. String, not Sting/1. 2. 3. Lowercase/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lowercase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the string");
+            string str = Console.ReadLine();
+            string[] words = str.Split(' ');
+            int LowerCaseWords = 0;
+
+            foreach (string word in words)
+            {
+                if (word.Equals(word.ToLower())) LowerCaseWords++;
+            }
+
+            Console.WriteLine("Total count: " + LowerCaseWords);
+
+            Console.ReadKey();
+
+        }
+    }
+}
