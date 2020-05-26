edaa3449d2f9c0594631f96ad7a1f585d60f4590
EkaterinaRadchenko <ekatherinered@gmail.com>
2020-05-26 21:29:19 +0400

    Added 1.2.2

---
+++ 1.2. String, not Sting/1. 2. 2. Doubler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doubler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input first string:");
+            StringBuilder StrToDouble = new StringBuilder(Console.ReadLine());
+            Console.WriteLine("Input second string:");
+            string StrDoubling = Console.ReadLine();
+
+            for (int i = 0; i < StrToDouble.Length; i++)
+                if (StrDoubling.Contains(StrToDouble[i])) {
+                    StrToDouble.Insert(i, StrToDouble[i]);
+                    i++;
+                }
+
+            Console.WriteLine("Result:");
+            Console.WriteLine(StrToDouble);
+            Console.ReadKey();
+        }
+    }
+}
