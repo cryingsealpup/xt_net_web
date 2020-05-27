733e0eeaf72996dcd30a7dff0e8a716f06bda049
EkaterinaRadchenko <ekatherinered@gmail.com>
2020-05-27 15:05:04 +0400

    Extended 1.1.7

--- 1. 1. The Magnificent Ten/1. 1. 7. Array processing/ArrProc.cs
+++ 1. 1. The Magnificent Ten/1. 1. 7. Array processing/ArrProc.cs
@@ -10,16 +10,20 @@ namespace Array_processing
     class ArrProc
     {
         private static Random _Rnd = new Random();
-        public static val GetRandomColors<val> ()
+        public static val GetRandomColors<val> () // array filling by enums
         {
             Array color = Enum.GetValues(typeof(val));
             return (val) color.GetValue(_Rnd.Next(color.Length));
         }
 
-
-        static string[] Sorting(string[] array)
+        public static int GetRandomNumber() // array filling by integers
         {
-            bool flag = true; // simple bubble sort
+            return _Rnd.Next(1, 1000);
+        }
+
+        static string[] BubbleSort(string[] array)
+        {
+            bool flag = true; // simple bubble sort for colors array
             while (flag)
             {
                 flag = false;
@@ -38,28 +42,70 @@ namespace Array_processing
             return array;
         }
 
+        static int[] SelectionSorting(int[] arr) // selection sort for integers array
+        {
+            int N = arr.Length;
+
+            for (int i = 0; i < N - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < N; j++)
+                    if (arr[j] < arr[min])
+                        min = j;
+                int temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
+            }
+
+            return arr;
+        }
 
         static void Main(string[] args)
         {
             Console.Write("Input lenght of an array: ");
             int N = int.Parse(Console.ReadLine());
-            string[] arr = new string[N];
+            string[] ColArr = new string[N];
+            int[] IntArr = new int[N];
+            Console.Write("Choose type of an array (1 for colors filling, 2 for integers filling): ");
+            int type = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("\nRandomly filled w {0} colors array: ", N);
-            for (int i = 0; i < N; i++)
+            if (type == 1)
             {
-                var value = GetRandomColors<Colors>();
-                arr[i] = value.ToString();
-                Console.Write(arr[i] + " ");
+                Console.WriteLine("\nRandomly filled w {0} colors array: ", N);
+                for (int i = 0; i < N; i++)
+                {
+                    var value = GetRandomColors<Colors>();
+                    ColArr[i] = value.ToString();
+                    Console.Write(ColArr[i] + " ");
+                }
+
+                ColArr = BubbleSort(ColArr);
+
+                Console.WriteLine("\n\nWhen sorted: ");
+                foreach (string elem in ColArr) Console.Write(elem + " ");
+
+                Console.WriteLine("\n\nmin = {0}, max = {1}", ColArr[0], ColArr[N - 1]);
             }
 
-            arr = Sorting(arr);
+            else if (type == 2)
+            {
+                Console.WriteLine("\nRandomly filled w {0} integers array: ", N);
+                for (int i = 0; i < N; i++)
+                {
+                    int value = GetRandomNumber();
+                    IntArr[i] = value;
+                    Console.Write(IntArr[i] + " ");
+                }
 
-            Console.WriteLine("\n\nWhen sorted: ");
-            foreach (string elem in arr) Console.Write(elem + " ");
+                IntArr = SelectionSorting(IntArr);
 
-            Console.WriteLine("\n\nmin = {0}, max = {1}", arr[0], arr[N - 1]);
+                Console.WriteLine("\n\nWhen sorted: ");
+                foreach (int elem in IntArr) Console.Write(elem + " ");
 
+                Console.WriteLine("\n\nmin = {0}, max = {1}", IntArr[0], IntArr[N - 1]);
+            }
+
+            else Console.WriteLine("Incorrect input");
             Console.ReadKey();
         }
     }
