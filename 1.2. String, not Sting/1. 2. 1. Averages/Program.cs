3dcf9ac405efaeb3356fec37fde71d4dbb09f833
EkaterinaRadchenko <ekatherinered@gmail.com>
2020-05-27 13:02:55 +0400

    Update 1. 2. 1

--- 1.2. String, not Sting/1. 2. 1. Averages/Program.cs
+++ 1.2. String, not Sting/1. 2. 1. Averages/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Averages
 {
@@ -7,13 +8,16 @@ namespace Averages
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Input the string:");
-            string str = Console.ReadLine().ToUpper();
+            /*Console.WriteLine("Input the string:");
+            string str = Console.ReadLine().ToUpper();*/
             // or test example
-            /* string str = "Викентий хорошо отметил день рождения: покушал пиццу, посмотрел кино, " +
-                "пообщался со студентами в чате".ToUpper(); */
-
-            string[] words = Regex.Split(str, @"\W+");
+            string str = "Викентий хорошо отметил день рождения: покушал пиццу, посмотрел кино, " +
+                "пообщался со студентами в чате".ToUpper();
+            List<char> separators = new List<char>();
+            foreach (char ch in str)
+                if (!(char.IsLetter(ch))) separators.Add(ch);
+            string[] words = str.Split(separators.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+            
             double sum = 0; // average length will be presented as double
             double WordsLen = 0;
 
